2c673c76016f0a260b3b26be3e1539eb7c04ca9c
Pablo Reyes <pablo288reyes@gmail.com>
2025-04-15 21:03:36 +0200

    Iterations 1 & 2 Done

--- js/index.js
+++ js/index.js
@@ -1,83 +1,69 @@
-// Write your Pizza Builder JavaScript in this file.
+const pepButton = document.querySelector(".btn-pepperoni");
 
-// Constants
-const basePrice = 10;
-const ingredients = {
-  pepperoni: { name: 'pepperoni', price: 1 },
-  mushrooms: { name: 'Mushrooms', price: 1 },
-  greenPeppers: { name: 'Green Peppers', price: 1 },
-  whiteSauce: { name: 'White sauce', price: 3 },
-  glutenFreeCrust: { name: 'Gluten-free crust', price: 5 }
+pepButton.onclick = function () {
+  pepButton.classList.toggle("active");
+
+  const peps = document.getElementsByClassName("pep");
+
+  
+
+  for (const pep of peps) {
+    pep.classList.toggle("hidden");
+  }
+  
+  ingredientsActive.pepperoni = !ingredientsActive.pepperoni;
+
+  calculateCosts();
 };
 
-// Initial value of the state (the state values can change over time)
-const state = {
+const costs = {
+  pepperoni: 1,
+  mushrooms: 1,
+  greenPepers: 1,
+  whiteSauce: 3,
+  glutenFreeCrust: 5
+};
+
+const ingredientsActive = {
   pepperoni: true,
   mushrooms: true,
-  greenPeppers: true,
-  whiteSauce: false,
-  glutenFreeCrust: false
+  greenPepers: true,
+  whiteSauce: true,
+  glutenFreeCrust: true
 };
 
-// This function takes care of rendering the pizza based on the state
-// This function is triggered once at the beginning and every time the state is changed
-function renderEverything() {
-  renderPepperoni();
-  renderMushrooms();
-  renderGreenPeppers();
-  renderWhiteSauce();
-  renderGlutenFreeCrust();
+function calculateCosts() {
+  const list = document.querySelector(".panel.price > ul");
 
-  renderButtons();
-  renderPrice();
-}
+  list.innerHTML = "";
 
-function renderPepperoni() {
-  document.querySelectorAll('.pep').forEach((onePep) => {
-    if (state.pepperoni) {
-      onePep.style.visibility = 'visible';
-    } else {
-      onePep.style.visibility = 'hidden';
+  const finalCost = document.querySelector(".panel.price > strong");
+  
+
+  let total = 10;
+  Object.keys(ingredientsActive).forEach(ing => {
+    if (ingredientsActive[ing]) {
+      total += costs[ing];
+
+      const li = document.createElement("li");
+      li.innerText = `${ing}: $ ${costs[ing]}`;
+
+      list.append(li);
     }
+    
+  
   });
+  finalCost.innerText = `$ ${total}`;
 }
 
-function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+calculateCosts();
+
+function onClickButton(ingredientClass) {
+  this.classList.toggle("active");
+
+  const ing = document.getElementsByClassName(ingredeintClass);
+
+  for (const ing of ings) {
+    ing.classList.toggle("hidden");
+  }
 }
-
-function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
-}
-
-function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
-
-function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
-}
-
-function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-}
-
-function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
-}
-
-renderEverything();
-
-// Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
-document.querySelector('.btn.btn-pepperoni').addEventListener('click', function () {
-  state.pepperoni = !state.pepperoni;
-  renderEverything();
-});
-
-// Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
-// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
-// Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
-
-// Iteration 2: Add click event listener on `<button class="btn btn-crust">`
